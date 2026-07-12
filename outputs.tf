f7c8a29219d8fe47738ac24a2fc8ed6ed9248939
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:38 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "spring_cloud_elastic_application_performance_monitorings" {
-  description = "All spring_cloud_elastic_application_performance_monitoring resources"
-  value       = azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings
-}
 output "spring_cloud_elastic_application_performance_monitorings_application_packages" {
-  description = "List of application_packages values across all spring_cloud_elastic_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : v.application_packages]
+  description = "Map of application_packages values across all spring_cloud_elastic_application_performance_monitorings, keyed the same as var.spring_cloud_elastic_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : k => v.application_packages }
 }
 output "spring_cloud_elastic_application_performance_monitorings_globally_enabled" {
-  description = "List of globally_enabled values across all spring_cloud_elastic_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : v.globally_enabled]
+  description = "Map of globally_enabled values across all spring_cloud_elastic_application_performance_monitorings, keyed the same as var.spring_cloud_elastic_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : k => v.globally_enabled }
 }
 output "spring_cloud_elastic_application_performance_monitorings_name" {
-  description = "List of name values across all spring_cloud_elastic_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : v.name]
+  description = "Map of name values across all spring_cloud_elastic_application_performance_monitorings, keyed the same as var.spring_cloud_elastic_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : k => v.name }
 }
 output "spring_cloud_elastic_application_performance_monitorings_server_url" {
-  description = "List of server_url values across all spring_cloud_elastic_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : v.server_url]
+  description = "Map of server_url values across all spring_cloud_elastic_application_performance_monitorings, keyed the same as var.spring_cloud_elastic_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : k => v.server_url }
 }
 output "spring_cloud_elastic_application_performance_monitorings_service_name" {
-  description = "List of service_name values across all spring_cloud_elastic_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : v.service_name]
+  description = "Map of service_name values across all spring_cloud_elastic_application_performance_monitorings, keyed the same as var.spring_cloud_elastic_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : k => v.service_name }
 }
 output "spring_cloud_elastic_application_performance_monitorings_spring_cloud_service_id" {
-  description = "List of spring_cloud_service_id values across all spring_cloud_elastic_application_performance_monitorings"
-  value       = [for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : v.spring_cloud_service_id]
+  description = "Map of spring_cloud_service_id values across all spring_cloud_elastic_application_performance_monitorings, keyed the same as var.spring_cloud_elastic_application_performance_monitorings"
+  value       = { for k, v in azurerm_spring_cloud_elastic_application_performance_monitoring.spring_cloud_elastic_application_performance_monitorings : k => v.spring_cloud_service_id }
 }
 
